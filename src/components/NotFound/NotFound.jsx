758c693654775414b9ed448f94a8511d5f4c758a
TanvirCoU <ahmeadtanvir@stud.cou.ac.bd>
2023-12-21 02:50:52 +0600

    notfound page edited

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
             <div className='bg-gray-100 h-screen text-4xl text-red-600 text-center font-bold pt-20'>
                 <h1>404! <br /> Error!</h1>
                 <div className="text-blue-600">
-                    Instead Go To <Link to="http://127.0.0.1:5173/"><p className="underline">http://127.0.0.1:5173/</p></Link>
+                    Instead Go To <Link to="https://panda-book.netlify.app/"><p className="underline">https://panda-book.netlify.app</p></Link>
                 </div>
             </div>
 
